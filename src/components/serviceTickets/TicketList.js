f3d068419538302c396599fea119148b9ed34b31
CassieTesauro <cassie.tesauro@gmail.com>
2021-08-30 13:52:16 -0500

    added final chapter 7 notes

--- src/components/serviceTickets/TicketList.js
+++ src/components/serviceTickets/TicketList.js
@@ -4,7 +4,7 @@ export const TicketList = () => {
     const [tickets, updateTickets] = useState([])
 
     useEffect(
-        () => {
+        () => { //in API in browser, added ?_expand=employee&_expand=customer to make the matching customer and employee's objects appear for easier access with dot notation in the return statement at the bottom.  So the customerId and employeeId foreign keys auto matched and expanded to show the objects.
             fetch("http://localhost:8088/serviceTickets?_expand=employee&_expand=customer")
                 .then(res => res.json())
                 .then((serviceTicketsFromAPI) => {
