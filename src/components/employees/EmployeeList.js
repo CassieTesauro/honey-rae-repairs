c7071f1296f426fa7ab10faf0bf4d1903f40548c
CassieTesauro <cassie.tesauro@gmail.com>
2021-08-30 11:05:06 -0500

    code and notes chapter 5

--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -4,9 +4,8 @@ import React, { useEffect, useState } from "react" //"use__ hooks" are built-in 
 
 export const EmployeeList = () => { 
    
-    const [employees, setEmployees] = useState([])  
-
-    useEffect(  
+    const [employees, setEmployees] = useState([])  //const [stateVariable, functionToBeUsedOnTheStateVariable] = useStateHook(variable's intitial value aka initial state 'change' 
+    useEffect(  //use effect is like fetch get set alltogether.  2 parameters:  what function should run, and an array holding the state value of when it should run.
         () => {
             fetch("http://localhost:8088/employees") 
                 .then(res => res.json())  
@@ -17,7 +16,7 @@ export const EmployeeList = () => {
         []
     )
 
-    return(  
+    return(  //html must be in a return in react.  Everything in one fragment so we're technically only returning 1 thing.
       <>
             { 
                 employees.map(
