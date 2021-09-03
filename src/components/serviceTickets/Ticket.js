aefb64369e6f4ec2b7936b1a99fdcc9ac1a519f7
CassieTesauro <cassie.tesauro@gmail.com>
2021-09-03 15:48:53 -0500

    ticket.js boiler plate code

---
+++ src/components/serviceTickets/Ticket.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export const Ticket = () => {
+    return (
+       <>
+        <h2>Ticket Details</h2>
+        <section className="ticket">
+            <h3 className="ticket_description"></h3>
+            <div className="ticket_customer"></div>
+            <div className="ticket_employee"></div>
+        </section>
+       </> 
+    )
+}
--- src/components/serviceTickets/TicketList.js
+++ src/components/serviceTickets/TicketList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useHistory } from "react-router-dom"
+import { Link } from "react-router-dom"
 import './Tickets.css';
 
 export const TicketList = () => {
@@ -28,7 +29,7 @@ export const TicketList = () => {
                     (ticket) => {
                         return <div key={`ticket--${ticket.id}`}> 
                             <p className={ticket.emergency ? "emergency" : "ticket"}>  
-                                {ticket.emergency ? "🚑" : ""} {ticket.description} submitted by {ticket.customer.name} and worked on by {ticket.employee.name}
+                                {ticket.emergency ? "🚑" : ""} <Link to={`/tickets/${ticket.id}`}>{ticket.description}</Link> submitted by {ticket.customer.name} and worked on by {ticket.employee.name}
                             </p>
 
                         </div>
